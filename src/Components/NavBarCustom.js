6c2518461e0774a3d9aec8e78266a408b9542d71
igorgoshev <goshev.ig@gmail.com>
2024-03-05 23:37:00 +0100

    Home Page

--- src/Components/NavBarCustom.js
+++ src/Components/NavBarCustom.js
@@ -11,12 +11,14 @@ function NavBarCustom() {
         <Navbar expand="lg" className="container-fluid">
             <Container >
                 <Navbar.Brand className="align-content-center" href='#home'>
-
-                        <img src={logo} alt="logo" style={{maxHeight: "2.5em"}} className="px-4"/>
-
-                        DENTAL
-
-                        TOURISM
+                    <Row>
+                        <Col className="col-lg-5 col-sm-5 justify-content-center align-content-center">
+                            <img src={logo} alt="logo" style={{maxHeight: "2.5em"}} className="px-4"/>
+                        </Col>
+                        <Col className="justify-content-center align-content-center text-start text-wrap" style={{width: "4em"}}>
+                            DENTAL TOURISM
+                        </Col>
+                    </Row>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
@@ -26,7 +28,7 @@ function NavBarCustom() {
                         <Nav.Link className='px-3' href="#link">Services</Nav.Link>
                         <Nav.Link className='px-3' href="#link">Contact</Nav.Link>
                         <div className="px-3 justify-content-center">
-                            <Button className="border-0  text-body-white rounded-0" style={{backgroundColor: '#fd9a49', boxShadow: '0.125em 0.2em 0.4em'}}>Request Appointment</Button>
+                            <Button className="border-0  text-body-white rounded-0" style={{backgroundColor: '#fd9a49', boxShadow: '0.125em 0.2em 0.4em gray'}}>Request Appointment</Button>
                         </div>
                     </Nav>
                 </Navbar.Collapse>
