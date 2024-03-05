57722f634ab38f0fca0cebec765662f66ea194f9
igorgoshev <goshev.ig@gmail.com>
2024-03-05 22:35:41 +0100

    Added Components and Image
    Navbar and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBarCustom from "./Components/NavBarCustom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <NavBarCustom></NavBarCustom>
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
