89c16964e25643a8db44c8c749e682b58b9f955b
krsteva <milakrsteva@yahoo.com>
2024-03-06 17:53:51 +0100

    blue background

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,25 +3,34 @@ import doctor from "../images/dental-doctor.jpg"
 import Container from "react-bootstrap/Container";
 import Kocka from "../Components/Kocka";
 import {Image} from "react-bootstrap";
-import blue from "../images/background blue.png"
+import blue from "../images/background-blue.png"
 function Home() {
     return (
         <>
-                <Container fluid className="p-0" style={{
-                    backgroundImage: ` linear-gradient(to bottom, rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${doctor})`,
-                    backgroundRepeat:"no-repeat",
-                    backgroundAttachment: "fixed",
-                    height: '100vh',
-                    backgroundPosition: "center",
+            <Container fluid className="p-0 d-block" style={{
+                backgroundImage: ` linear-gradient(to bottom, rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(${doctor})`,
+                backgroundRepeat:"no-repeat",
+                backgroundAttachment: "fixed",
+                height: '100vh',
+                backgroundPosition: "center",
 
-                    // width: "100vw",
-                backgroundSize: "100% auto cover"}}>
+                // width: "100vw",
+            backgroundSize: "100% auto"}}>
+            </Container>
 
+            <Container fluid  style={{
+                backgroundImage: `url(${blue})`,
+                backgroundRepeat:"no-repeat",
+                backgroundAttachment: "fixed",
+                height: '100vh',
+                backgroundPosition: "center",
+                backgroundSize: "100% auto",
+                opacity: "30%"
+            }}>
+                {/* Content for the Second Container */}
+                <p>laaa</p>
+            </Container>
 
-                </Container>
-
-
-            
             <Kocka></Kocka>
             <Kocka></Kocka>
 
