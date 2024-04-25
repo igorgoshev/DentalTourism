af47922927d6249ae945f4b44f50c12519171016
krsteva <milakrsteva@yahoo.com>
2024-04-25 17:15:50 +0200

    FAQ

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,7 +1,16 @@
+import FAQ from "../Components/FAQ";
+import Footer from "../Components/Footer";
+import Container from "react-bootstrap/Container";
+import React from "react";
 
 function AboutUs() {
     return (
         <>
+            <Container style={{width: "800px"}}>
+
+                <FAQ></FAQ>
+            </Container>
+
         </>
     )
 }
