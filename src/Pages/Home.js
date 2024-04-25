6852464be87fad1720c31110849c84f18d4efcbc
krsteva <milakrsteva@yahoo.com>
2024-04-25 14:23:23 +0200

    Update Location

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ import {Col, Image, Row} from "react-bootstrap";
 import blue from "../images/background-blue.png"
 import Footer from "../Components/Footer";
 import BeforeAfter from "../Components/BeforeAfter";
+import {Link} from "react-router-dom";
 function Home() {
     return (
         <>
@@ -70,18 +71,43 @@ function Home() {
             </Container>
 
             <Container>
-                <Row>
-
-
-                <Col style={{width: "100%"}} className="m-8">
-                    <iframe style={{width:"800px", height:"600px"}}
-                            src="https://maps.google.com/maps?width=800&amp;height=600&amp;hl=en&amp;q=Majka%20Tereza%2060/3+(dr%20Ljupka%20Terzieva)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                        <a href="https://www.gps.ie/">gps systems</a></iframe>
-                </Col>
+                <Row className="py-5">
+                    <Col style={{width: "100%"}} className="m-8">
+                        <iframe style={{width:"800px", height:"700px"}}
+                                src="https://maps.google.com/maps?width=800&amp;height=600&amp;hl=en&amp;q=Majka%20Tereza%2060/3+(dr%20Ljupka%20Terzieva)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                            <a href="https://www.gps.ie/">gps systems</a></iframe>
+                    </Col>
                     <Col className="m-4">
-                    <address>
-                        
-                    </address>
+
+                        <Col className="pt-0 px-lg-5 pb-0 display-4 " style={{color:"#FF9849", fontWeight:"bold", opacity: "30%"}}>LOCATION </Col>
+                        <Col className="px-0" style={{color:"#FF9849", fontWeight:"bold", fontSize: "2rem", marginTop: "-30px" }}>Our dental office </Col>
+
+                        <Container className="m-5 d-flex text-start">
+                           <Col>
+                               <h5>Address</h5>
+                               <p className="col-4">Majka Tereza 60/3, Skopje 1000</p>
+                               <Link href="https://maps.app.goo.gl/cHLHtASJj9BHP9HC8">Get directions</Link>
+                           </Col>
+                        </Container>
+
+                        <hr/>
+                        <Container className="m-5 d-flex text-start">
+                            <Col>
+                                <h5>Phone</h5>
+                                <p className="col-4">070/000-000</p>
+                            </Col>
+                        </Container>
+
+                        <hr/>
+                        <Container className="m-5 d-flex text-start">
+                            <Col>
+                                <h5>Open Hours</h5>
+                                <h6>Monday-Friday: </h6>
+                                <p>8am–5pm</p>
+                            </Col>
+                        </Container>
+
+                        <hr/>
                     </Col>
                 </Row>
             </Container>
