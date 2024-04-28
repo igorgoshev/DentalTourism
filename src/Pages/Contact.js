e160e9da4385c8c93b32f548c9c5e1b742291bb9
krsteva <milakrsteva@yahoo.com>
2024-04-28 21:55:56 +0200

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -11,11 +11,11 @@ function Contact() {
                 backgroundImage: ` linear-gradient(to bottom, rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.60)), url(https://www.leica-microsystems.com/fileadmin/_processed_/d/3/csm_dental-applications_8c3fb35730.jpg)`,
                 backgroundRepeat:"no-repeat",
                 backgroundAttachment: "fixed",
-                height: '65vh',
+                height: '64vh',
                 backgroundSize: "100% auto"}}>
                 <Row style={{height: "100%"}} className="align-items-center">
                     <Col className="col-12 d-flex justify-content-center p-5" >
-                        <h1 className="text-white align-self-center text-center col-8"  style={{paddingTop: "150px"}}>Book your free initial consultation today and take the first step towards a healthier smile!</h1>
+                        <h1 className="text-white text-center col-8"  style={{paddingTop: "150px"}}>Book your free initial consultation today and take the first step towards a healthier smile!</h1>
                     </Col>
                     <Col className="col-12 align-self-end text-start text-white" style={{paddingBottom: "4em", paddingLeft: "4em"}}>
 
